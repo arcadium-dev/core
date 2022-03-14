b3e3adda7e914c44af806eca485b722aba57b444
Ian Cahoon <167669+icahoon@users.noreply.github.com>
2022-03-13 22:09:17 -0600

    refactor http response

--- http/response.go
+++ http/response.go
@@ -15,7 +15,9 @@
 package http
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -23,73 +25,69 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 
+	cerrors "arcadium.dev/core/errors"
 	"arcadium.dev/core/log"
 )
 
-// Response writes http error responses to the http.ResponseWriter. The first
-// error's status will be used to write the http response header.
-func Response(w http.ResponseWriter, errs ...ResponseError) {
+// Response writes an http error responses to the http.ResponseWriter.
+func Response(ctx context.Context, w http.ResponseWriter, err error) {
+	if err == nil {
+		return
+	}
+
+	logger := log.LoggerFromContext(ctx)
+
+	switch {
+	case errors.Is(err, cerrors.ErrInvalidArgument):
+		logger.Warn("reason", err.Error())
+		response(ctx, w, http.StatusBadRequest, err)
+
+	case errors.Is(err, cerrors.ErrNotFound):
+		logger.Warn("reason", err.Error())
+		response(ctx, w, http.StatusNotFound, err)
+
+	case errors.Is(err, cerrors.ErrAlreadyExists):
+		logger.Warn("reason", err.Error())
+		response(ctx, w, http.StatusConflict, err)
+
+	case errors.Is(err, cerrors.ErrNotImplemented):
+		logger.Error("error", err.Error())
+		response(ctx, w, http.StatusNotImplemented, err)
+
+	default:
+		logger.Error("error", err.Error())
+		response(ctx, w, http.StatusInternalServerError, err)
+	}
+}
+
+func response(ctx context.Context, w http.ResponseWriter, status int, e error) {
+	err := responseError{Status: status}
+	if e != nil {
+		err.Detail = e.Error()
+	}
+	w.WriteHeader(err.Status)
+
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 
-	for i := 0; i < len(errs); i++ {
-		if errs[i].Status < 100 || 999 < errs[i].Status {
-			errs[i].Status = http.StatusInternalServerError
-		}
-		if i == 0 {
-			w.WriteHeader(errs[i].Status)
-		}
-		httpErrorCount.WithLabelValues(strconv.Itoa(errs[i].Status)).Inc()
+	httpErrorCount.WithLabelValues(strconv.Itoa(err.Status)).Inc()
+
+	resp := struct {
+		Error responseError `json:"error,omitempty"`
+	}{
+		Error: err,
 	}
-
-	if len(errs) > 0 {
-		resp := struct {
-			Errors []ResponseError `json:"errors,omitempty"`
-		}{
-			Errors: errs,
-		}
-		if err := json.NewEncoder(w).Encode(resp); err != nil {
-			log.Error("msg", "unable to write error response", "error", err.Error())
-		}
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		log.LoggerFromContext(ctx).Error(
+			"msg", "unable to write error response", "error", err.Error(),
+		)
 	}
-}
-
-// BadRequestError creates a ResponseError with the status of
-// http.StatusBadRequest.
-func BadRequestError(err error) ResponseError {
-	return wrap(http.StatusBadRequest, err)
-}
-
-// NotFoundError creates a ResponseError with the status of
-// http.StatusNotFound.
-func NotFoundError(err error) ResponseError {
-	return wrap(http.StatusNotFound, err)
-}
-
-// ConflictError creates a ResponseError with the status of
-// http.StatusConflict.
-func ConflictError(err error) ResponseError {
-	return wrap(http.StatusConflict, err)
-}
-
-// InternalServerError creates a ResponseError with the status of
-// http.StatusInternalServerError.
-func InternalServerError(err error) ResponseError {
-	return wrap(http.StatusInternalServerError, err)
-}
-
-// NotImplementedError creates a ResponseError with the status of
-// http.StatusNotImplemented.
-func NotImplementedError(err error) ResponseError {
-	return wrap(http.StatusNotImplemented, err)
 }
 
 type (
-	// ResponseError provides additional informatio about problems encounted
-	// while performing an operation.
-	//
-	// See: https://jsonapi.org/format/#error-objects
-	ResponseError struct {
+	// responseError provides additional information about problems encounted while
+	// performing an operation. See: https://jsonapi.org/format/#error-objects
+	responseError struct {
 		// Status is the http status code applicable to this problem.
 		Status int `json:"status,omitempty"`
 		// Detail is a human-readable explanation specific to this occurrence of
@@ -99,7 +97,7 @@ type (
 )
 
 // Error translates the error to a string.
-func (e ResponseError) Error() string {
+func (e responseError) Error() string {
 	return fmt.Sprintf("status=%d, detail=%q", e.Status, e.Detail)
 }
 
@@ -109,11 +107,3 @@ var (
 		Help: "Total number of http errors by error status.",
 	}, []string{"status"})
 )
-
-func wrap(status int, err error) ResponseError {
-	rerr := ResponseError{Status: status}
-	if err != nil {
-		rerr.Detail = err.Error()
-	}
-	return rerr
-}
