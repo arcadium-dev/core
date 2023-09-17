8302152d73129c514ab293a729b295ad45a9402b
Ian Cahoon <167669+icahoon@users.noreply.github.com>
2023-09-16 20:09:40 -0600

    Issue #1: create core repo

---
+++ build/build.go
@@ -0,0 +1,45 @@
+// Copyright 2023 arcadium.dev <info@arcadium.dev>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package build provide information about the build.
+package build // import "arcadium.dev/core/build"
+
+import (
+	"fmt"
+	"runtime"
+)
+
+// Information holds the build information.
+type Information struct {
+	Name, Version, Branch, Commit, Date, Go string
+}
+
+// Info populates the build information.
+func Info(n, v, b, c, d string) Information {
+	return Information{
+		Name:    n,
+		Version: v,
+		Branch:  b,
+		Commit:  c,
+		Date:    d,
+		Go:      runtime.Version(),
+	}
+}
+
+// String provides the build information as a string.
+func (i Information) String() string {
+	return fmt.Sprintf("%s %s (branch: %s, commit: %s, date: %s, go: %s)",
+		i.Name, i.Version, i.Branch, i.Commit, i.Date, i.Go,
+	)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module arcadium.dev/core
+
+go 1.21
+
+require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
+	github.com/google/go-cmp v0.5.9
+)
