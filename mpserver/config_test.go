e7e89ad81f795f04985f39bb10c3deb2e8fecb3b
Ian Cahoon <167669+icahoon@users.noreply.github.com>
2024-09-03 16:16:33 -0600

    Issue #55: add mpserver.Config (#56)
    * Issue #55: add mpserver.Config
    
    * code review fixes

---
+++ mpserver/config_test.go
@@ -0,0 +1,38 @@
+package mpserver_test
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"arcadium.dev/core/assert"
+	"arcadium.dev/core/mpserver"
+)
+
+func TestNewConfig(t *testing.T) {
+	for _, env := range []string{"LOG_LEVEL", "SHUTDOWN_TIMEOUT"} {
+		os.Unsetenv(env)
+	}
+
+	t.Run("test defaults", func(t *testing.T) {
+		cfg, err := mpserver.NewConfig()
+
+		assert.Nil(t, err)
+		assert.Equal(t, cfg.LogLevel(), mpserver.DefaultLogLevel)
+		assert.Equal(t, cfg.ShutdownTimeout(), mpserver.DefaultShutdownTimeout)
+	})
+
+	t.Run("success", func(t *testing.T) {
+		expectedLogLevel := "warn"
+		expectedShutdownTimeout := 111 * time.Second
+
+		t.Setenv("LOG_LEVEL", expectedLogLevel)
+		t.Setenv("SHUTDOWN_TIMEOUT", "111s")
+
+		cfg, err := mpserver.NewConfig()
+
+		assert.Nil(t, err)
+		assert.Equal(t, cfg.LogLevel(), expectedLogLevel)
+		assert.Equal(t, cfg.ShutdownTimeout(), expectedShutdownTimeout)
+	})
+}
